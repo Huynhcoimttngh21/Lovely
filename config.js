7da0512c0ba45dbca0571cbe8c088bded5f94105
Huynhcoimttngh21 <Huynhcoimttnhp@gmail.com>
2024-05-11 15:59:20 +0700

    commit

---
+++ config.js
@@ -0,0 +1,22 @@
+// 霸都丶傲天 2019年12月24日
+
+var config = {
+  // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+  // 每句话尽量不要超过15个字,不然展示效果可能不太好
+  texts: [
+    "Dear Trong Viet", //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+    "My lovely boy", // 同上...
+    "Today is a normal day like any other day",
+    "I still remember the first words we texted each other",
+    "I want to be with you for a long time",
+    "I also want you to stay by my side for a much longer time",
+    "Ok!",
+    "The lyrics are the words I want to tell you",
+
+    "Tien Huynh~~",
+  ],
+  wish: "The nice guy and the stupid guy fall in love", //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+  // 时间的格式很重要哦，一定要是下面这个格式!!
+  // 年年年年-月月-日日 时时:分分:秒秒
+  time: "2024-04-01 0:0:0", //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+};
